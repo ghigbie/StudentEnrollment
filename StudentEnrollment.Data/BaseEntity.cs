0bf648ceb425097f17854fd68f2d5cac7118bd09
George Higbie <gehigbie@gmail.com>
2023-02-11 18:07:37 -0500

    Add classes

---
+++ StudentEnrollment.Data/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentEnrollment.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifieBy { get; set; }
+    }
+
+}
